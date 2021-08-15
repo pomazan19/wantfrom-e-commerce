29bf4ff2d3486c2f1e5f03275bc4bc67cbc2b68b
pomazan19 <poma205@gmail.com>
2021-08-15 12:24:20 -0300

    layout project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent }, //returns an error
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
